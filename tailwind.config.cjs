787a2d4e51e430c59a73417956ce240fb376ddc0
Mathea Hansen <math21t7@stud.kea.dk>
2022-10-13 21:13:43 +0200

    test sitemap

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{html,vue,js,ts,jsx,tsx,xml}"],
   theme: {
     colors: {
       red:'#830101',
