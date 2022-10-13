62fc739a68fd5ceb8d4c198cdd88d35f5780a2de
Mathea Hansen <math21t7@stud.kea.dk>
2022-10-13 21:57:25 +0200

    sitemap change

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,6 @@ export default defineConfig({
         sitemap: resolve(__dirname, "src/sitemap.html"),
         kontakt: resolve(__dirname, "src/Kontakt.html"),
         vildmedmig: resolve(__dirname, "src/vildmedmig.html"),
-        sitemapxml: resolve(__dirname, "src/sitemap.xml"),
 
         // about: resolve(__dirname, "src/about.html"),
       },
