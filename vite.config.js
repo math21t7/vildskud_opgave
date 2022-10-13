c6af69b6c3775c894557db8f7f92384d6e50165d
Mathea Hansen <math21t7@stud.kea.dk>
2022-10-13 17:51:44 +0200

    tilføjet alle html sider til config

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,22 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        billetter: resolve(__dirname, "src/billetter.html"),
+        events: resolve(__dirname, "src/events.html"),
+        arkiv: resolve(__dirname, "src/arkiv.html"),
+        program: resolve(__dirname, "src/program.html"),
+        partout: resolve(__dirname, "src/partout.html"),
+        endag: resolve(__dirname, "src/endag.html"),
+        om: resolve(__dirname, "src/om.html"),
+        vildmedmig: resolve(__dirname, "src/vildmedmig.html"),
+        maskeret: resolve(__dirname, "src/maskeret.html"),
+        frivillige: resolve(__dirname, "src/frivillige.html"),
+        opencall: resolve(__dirname, "src/opencall.html"),
+        cookies: resolve(__dirname, "src/cookies.html"),
+        privateliv: resolve(__dirname, "src/privateliv.html"),
+        sitemap: resolve(__dirname, "src/sitemap.html"),
+        kontakt: resolve(__dirname, "src/kontakt.html"),
+
         // about: resolve(__dirname, "src/about.html"),
       },
     },
