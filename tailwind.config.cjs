c9dbc0596a5f6b5dd2fc81cd424a47fc88329648
Mathea Hansen <math21t7@stud.kea.dk>
2022-10-13 16:50:33 +0200

    test

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"] ["./billetter.html"],
   theme: {
     colors: {
       red:'#830101',
